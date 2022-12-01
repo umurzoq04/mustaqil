46f6f7d901d74ea6c6ecee929fa60df739918365
umurzoq04 <zoyirovumurzoq31@gmail.com>
2022-12-01 15:34:15 +0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,43 @@
+tailwind.config ={
+    theme:{
+        extend:{
+            colors:{
+                back:'#7872B9',
+                bgcolor:'#F9FCFF',
+                li:'#4D4D52;',
+                text:'#4D4D52',
+                pback:'#EBF5FF',
+                pushti:' #F1E6F7;',
+               
+            },
+            container:{
+                screens:{
+                    "2xl":"1140px",
+                    xl:"1024px",
+                    lg:"768px",
+                    md:"576px",
+                    sm:"384px",
+                  }
+            },
+            maxWidth: {
+                'buton': '220px',
+                'li-back':'280px',
+                'div':'680px',
+                'leg':'440px',
+                program:'480px',
+                foto:'250px'
+              },
+            fontFamily: {
+                'body': ['Montserrat',],
+        },
+        fontSize: {
+           sarlavha:'40px',
+           paket:'20px',
+          },
+          letterSpacing: {
+            test: '1px',
+         
+          },
+    }
+}
+}
